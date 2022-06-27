77e9807a5a2603c98b30b6af271e195b68ed8195
GordonGichini <gordongichini1@gmail.com>
2022-06-27 05:17:42 +0300

    added images

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 let search = document.querySelector (".search-box");
 
 document.querySelector("#search-icon").onclick = () => {
@@ -23,29 +23,4 @@ let Header = document.querySelector('header')
 window.addEventListener('scroll', () => {
     Header.classList.toggle('shadow', window.scroll > 0);
 });
-=======
-let search = document.querySelector(".search-box");
 
-document.querySelector("#search-icon").onclick = () => {
-    search.classList.toggle("active");
-    menu.classList.remove('active');
-}
-let menu = document.querySelector(".navbar");
-
-document.querySelector("#menu-icon").onclick = () => {
-    menu.classList.toggle("active");
-    search.classList.remove('active');
-}
-//Hide menu and Search bar
-window.onscroll = () => {
-    menu.classList.remove('active')
-    search.classList.remove('active')
-}
-
-//Header
-let Header = document.querySelector('header')
-
-window.addEventListener('scroll', () => {
-    Header.classList.toggle('shadow', window.scroll > 0);
-});
->>>>>>> 10777960cbd80faac04ea785a3b15c8aa818d619
